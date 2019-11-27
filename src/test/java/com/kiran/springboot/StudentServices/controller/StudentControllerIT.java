de9772ed158ff0b8298cd3a31e4910215d1afb65
Imkbhat <kbs71190@gmail.com>
2019-11-27 18:06:39 +0530

    Integration Testing

--- src/test/java/com/kiran/springboot/StudentServices/controller/StudentControllerIT.java
+++ src/test/java/com/kiran/springboot/StudentServices/controller/StudentControllerIT.java
@@ -1,5 +1,71 @@
 package com.kiran.springboot.StudentServices.controller;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kiran.springboot.StudentServices.StudentServicesApplication;
+import com.kiran.springboot.StudentServices.model.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StudentServicesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerIT {
+
+	@LocalServerPort
+	private int port;
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	HttpHeaders headers = new HttpHeaders();
+
+	@Test
+	public void testRetrieveStudentCourse() throws JSONException {
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+				createURLWithPort("/students/Student1/courses/Course1"),
+				HttpMethod.GET, entity, String.class);
+
+		String expected = "{id:Course1,name:Spring,description:10 Steps}";
+
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+	}
+
+	@Test
+	public void addCourse() {
+
+		Course course = new Course("Course1", "Spring", "10 Steps",
+				Arrays.asList("Learn Maven", "Import Project", "First Example",
+						"Second Example"));
+
+		HttpEntity<Course> entity = new HttpEntity<Course>(course, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+				createURLWithPort("/students/Student1/courses"),
+				HttpMethod.POST, entity, String.class);
+
+		String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+		assertTrue(actual.contains("/students/Student1/courses/"));
+
+	}
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
+	}
 
 }
